b164dfa776292bf5653acb3adf0de6eba35e8f9a
Beimarsempai <87555802+Beimarsempai@users.noreply.github.com>
2021-07-21 13:59:01 -0400

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,85 @@
+const help = (prefix) => {
+	return `⌜ *Lore* ⌟  
+
+❍➣ *INFORMACION*
+
+ Comando : ⌜ ${prefix} ⌟
+
+❍➣ ¿Tutoriales de crear bots?
+Responde esta encuesta: 
+
+❍➣ *NUEVOS MENUS*
+ │⊷️ ${prefix}desmenu
+ │Descargar musica y videos de YT
+ │⊷️ ${prefix}version
+ │Conoce la versión de tu bot
+ │⊷️ ${prefix}welmenu
+ │⊷️ Comandos de bienvedia a grupos
+ ╰❏
+
+❍➣ *CREAR STICKERS*
+  │
+  │⊷️ ${prefix}sticker
+  │⊷️ ${prefix}attp
+  │⊷️  Crea texto de colores
+  │⊷️ ${prefix}emoji 
+  │⊷️  Convierte un emoji a sticker
+  │⊷️ ${prefix}stickergif
+  │⊷️ 6 segundos de video
+  ╰❏ 
+
+❍➣ *CONVERTIDORES*
+  │
+  │⊷️ ${prefix}toimg
+  │⊷️  De sticker a JPG
+  │⊷️ ${prefix}tomp3
+  │⊷️ De video a MP3
+  ╰❏ 
+
+❍➣ *AUDIO*
+  │
+  │⊷️ ${prefix}idioma
+  │⊷️ ${prefix}tts es (No Funcional)
+  ╰❏
+
+❍➣ *OTROS*
+  │  
+  │⊷️ ${prefix}otak
+  │⊷️  Audios del bot
+  │⊷️ ${prefix}wame
+  │⊷️  Link de Whatsapp
+  │⊷️ ${prefix}qrcode
+  │⊷️ Coloca un texto
+  ╰❏ 
+
+❍➣ *GRUPOS*
+  │
+  │⊷️ ${prefix}closegc
+  │⊷️  Cerrar el grupo solo admins
+  │⊷️ ${prefix}opengc
+  │⊷️  Abrir grupo solo admins
+  │⊷️ ${prefix}kick o pafuera
+  │⊷️  Eliminar a un miembro 
+  │⊷️ ${prefix}promote
+  │⊷️  Dar admin a un miembro
+  │⊷️ ${prefix}demote
+  │⊷️  Quitar el admin
+  │⊷️ ${prefix}listadmin
+  │⊷️  Ver a todos los admins
+  │⊷️ ${prefix}hidetag
+  │⊷️  Menciona a todos en secreto
+  │⊷️ ${prefix}linkgc
+  │⊷️  Link del grupo
+  │⊷️ ${prefix}tagall
+  │⊷️ Nombra a todos los del grupo
+  ╰❏ 
+
+❏ Para usar estas funciones el bot necesita admin
+  
+❍➣ *NSWF* 
+  │⊷️ ${prefix}nsfwmenu
+  ╰❏
+`
+}
+
+exports.help = help
---
+++ src/version.js
@@ -0,0 +1,16 @@
+const version = (prefix, pushname) => {
+    return `
+*Lore*
+
+*Actualizado:* 19 De Julio
+*Versión actual:* 2
+*Ofrecida por:* Beimar
+
+
+
+by Beimar
+`
+
+}
+
+exports.version = version
